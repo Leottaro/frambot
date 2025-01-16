47632997cdaf3cb352485848d68a5e55ec8cf216
Leottaro <leo.hafdane@gmail.com>
2025-01-16 20:05:30 +0100

    my formatting

--- src/commands/menu.ts
+++ src/commands/menu.ts
@@ -1,59 +1,56 @@
 import type {
-  CommandInteraction,
-  MessageActionRowComponentBuilder,
-  StringSelectMenuInteraction,
+    CommandInteraction,
+    MessageActionRowComponentBuilder,
+    StringSelectMenuInteraction,
 } from "discord.js";
 import { ActionRowBuilder, StringSelectMenuBuilder } from "discord.js";
 import { Discord, SelectMenuComponent, Slash } from "discordx";
 
 const roles = [
-  { label: "Principal", value: "principal" },
-  { label: "Teacher", value: "teacher" },
-  { label: "Student", value: "student" },
+    { label: "Principal", value: "principal" },
+    { label: "Teacher", value: "teacher" },
+    { label: "Student", value: "student" },
 ];
 
 @Discord()
 export class Example {
-  @SelectMenuComponent({ id: "role-menu" })
-  async handle(interaction: StringSelectMenuInteraction): Promise<void> {
-    await interaction.deferReply();
+    @SelectMenuComponent({ id: "role-menu" })
+    async handle(interaction: StringSelectMenuInteraction): Promise<void> {
+        await interaction.deferReply();
 
-    // extract selected value by member
-    const roleValue = interaction.values[0];
+        // extract selected value by member
+        const roleValue = interaction.values[0];
 
-    // if value not found
-    if (!roleValue) {
-      await interaction.followUp("invalid role id, select again");
-      return;
+        // if value not found
+        if (!roleValue) {
+            await interaction.followUp("invalid role id, select again");
+            return;
+        }
+
+        await interaction.followUp(
+            `you have selected role: ${
+                roles.find((r) => r.value === roleValue)?.label ?? "unknown"
+            }`
+        );
     }
 
-    await interaction.followUp(
-      `you have selected role: ${
-        roles.find((r) => r.value === roleValue)?.label ?? "unknown"
-      }`,
-    );
-  }
+    @Slash({ description: "roles menu", name: "my_roles" })
+    async myRoles(interaction: CommandInteraction): Promise<unknown> {
+        await interaction.deferReply();
 
-  @Slash({ description: "roles menu", name: "my_roles" })
-  async myRoles(interaction: CommandInteraction): Promise<unknown> {
-    await interaction.deferReply();
+        // create menu for roles
+        const menu = new StringSelectMenuBuilder().addOptions(roles).setCustomId("role-menu");
 
-    // create menu for roles
-    const menu = new StringSelectMenuBuilder()
-      .addOptions(roles)
-      .setCustomId("role-menu");
+        // create a row for message actions
+        const buttonRow = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+            menu
+        );
 
-    // create a row for message actions
-    const buttonRow =
-      new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-        menu,
-      );
-
-    // send it
-    await interaction.editReply({
-      components: [buttonRow],
-      content: "select your role!",
-    });
-    return;
-  }
+        // send it
+        await interaction.editReply({
+            components: [buttonRow],
+            content: "select your role!",
+        });
+        return;
+    }
 }
--- src/commands/slashes.ts
+++ src/commands/slashes.ts
@@ -5,32 +5,32 @@ import { Discord, MetadataStorage, Slash } from "discordx";
 
 @Discord()
 export class SlashExample {
-  // example: pagination for all slash command
-  @Slash({
-    description: "Pagination for all slash command",
-    name: "all-commands",
-  })
-  async pages(interaction: CommandInteraction): Promise<void> {
-    const commands = MetadataStorage.instance.applicationCommands.map((cmd) => {
-      return { description: cmd.description, name: cmd.name };
-    });
-
-    const pages = commands.map((cmd, i) => {
-      const embed = new EmbedBuilder()
-        .setFooter({
-          text: `Page ${String(i + 1)} of ${String(commands.length)}`,
-        })
-        .setTitle("**Slash command info**")
-        .addFields({ name: "Name", value: cmd.name })
-        .addFields({
-          name: "Description",
-          value: cmd.description,
+    // example: pagination for all slash command
+    @Slash({
+        description: "Pagination for all slash command",
+        name: "all-commands",
+    })
+    async pages(interaction: CommandInteraction): Promise<void> {
+        const commands = MetadataStorage.instance.applicationCommands.map((cmd) => {
+            return { description: cmd.description, name: cmd.name };
         });
 
-      return { embeds: [embed] };
-    });
+        const pages = commands.map((cmd, i) => {
+            const embed = new EmbedBuilder()
+                .setFooter({
+                    text: `Page ${String(i + 1)} of ${String(commands.length)}`,
+                })
+                .setTitle("**Slash command info**")
+                .addFields({ name: "Name", value: cmd.name })
+                .addFields({
+                    name: "Description",
+                    value: cmd.description,
+                });
 
-    const pagination = new Pagination(interaction, pages);
-    await pagination.send();
-  }
+            return { embeds: [embed] };
+        });
+
+        const pagination = new Pagination(interaction, pages);
+        await pagination.send();
+    }
 }
