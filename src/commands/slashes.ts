bf24c5312f31692785206b77a8370cb76d08a9f6
Leottaro <leo.hafdane@gmail.com>
2025-01-17 12:50:17 +0100

    removed dockerx default features not needed

--- src/commands/slashes.ts
+++ src/commands/slashes.ts
@@ -1,36 +1,32 @@
-import { Pagination } from "@discordx/pagination";
 import type { CommandInteraction } from "discord.js";
-import { EmbedBuilder } from "discord.js";
-import { Discord, MetadataStorage, Slash } from "discordx";
+import { ApplicationCommandOptionType } from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
 
 @Discord()
-export class SlashExample {
-    // example: pagination for all slash command
-    @Slash({
-        description: "Pagination for all slash command",
-        name: "all-commands",
-    })
-    async pages(interaction: CommandInteraction): Promise<void> {
-        const commands = MetadataStorage.instance.applicationCommands.map((cmd) => {
-            return { description: cmd.description, name: cmd.name };
-        });
+export class SlashCommands {
+    @Slash({ name: "ping", description: "Respond with pong!" })
+    async ping(interaction: CommandInteraction): Promise<void> {
+        interaction.reply("Pong!");
+    }
 
-        const pages = commands.map((cmd, i) => {
-            const embed = new EmbedBuilder()
-                .setFooter({
-                    text: `Page ${String(i + 1)} of ${String(commands.length)}`,
-                })
-                .setTitle("**Slash command info**")
-                .addFields({ name: "Name", value: cmd.name })
-                .addFields({
-                    name: "Description",
-                    value: cmd.description,
-                });
-
-            return { embeds: [embed] };
-        });
-
-        const pagination = new Pagination(interaction, pages);
-        await pagination.send();
+    @Slash({ name: "add", description: "add x and y" })
+    async add(
+        @SlashOption({
+            description: "x value",
+            name: "x",
+            required: true,
+            type: ApplicationCommandOptionType.Number,
+        })
+        x: number,
+        @SlashOption({
+            description: "y value",
+            name: "y",
+            required: true,
+            type: ApplicationCommandOptionType.Number,
+        })
+        y: number,
+        interaction: CommandInteraction
+    ): Promise<void> {
+        interaction.reply(`${x} + ${y} = ${x + y}`);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Client } from "discordx";
 
 export const bot = new Client({
     // To use only guild command
-    // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+    botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
 
     // Discord intents
     intents: [
@@ -19,16 +19,11 @@ export const bot = new Client({
 
     // Debug logs are disabled in silent mode
     silent: false,
-
-    // Configuration for @SimpleCommand
-    simpleCommand: {
-        prefix: "!",
-    },
 });
 
-bot.once("ready", () => {
+bot.once("ready", async () => {
     // Make sure all guilds are cached
-    // await bot.guilds.fetch();
+    await bot.guilds.fetch();
 
     // Synchronize applications commands with Discord
     void bot.initApplicationCommands();
@@ -37,19 +32,13 @@ bot.once("ready", () => {
     // This is useful when moving from guild commands to global commands
     // It must only be executed once
     //
-    //  await bot.clearApplicationCommands(
-    //    ...bot.guilds.cache.map((g) => g.id)
-    //  );
+    // await bot.clearApplicationCommands(...bot.guilds.cache.map((g) => g.id));
 
     console.log("Bot started");
 });
 
 bot.on("interactionCreate", (interaction: Interaction) => {
     bot.executeInteraction(interaction);
-});
-
-bot.on("messageCreate", (message: Message) => {
-    void bot.executeCommand(message);
 });
 
 async function run() {
