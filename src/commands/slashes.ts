71e1435658ee64e586a79722b5be5464386cdb92
Leottaro <leo.hafdane@gmail.com>
2025-01-18 17:48:13 +0100

    youtubePlayer

--- src/commands/slashes.ts
+++ src/commands/slashes.ts
@@ -1,7 +1,9 @@
 import type { CommandInteraction } from "discord.js";
 import { ApplicationCommandOptionType } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
+import YoutubePlayer from "../utils/youtubePlayer.js";
 
+const youtube_player = new YoutubePlayer();
 @Discord()
 export class SlashCommands {
     @Slash({ name: "ping", description: "Respond with pong!" })
@@ -9,24 +11,106 @@ export class SlashCommands {
         interaction.reply("Pong!");
     }
 
-    @Slash({ name: "add", description: "add x and y" })
-    async add(
+    @Slash({ name: "join", description: "join the same channel the user is in" })
+    async join(interaction: CommandInteraction) {
+        const member = interaction.guild?.members.cache.get(interaction.user.id);
+        if (!member) {
+            interaction.reply("I can't find you");
+            return;
+        }
+
+        // join the voice channel
+        const voiceChannel = member.voice.channel;
+        if (!voiceChannel) {
+            interaction.reply("You need to be in a voice channel to use this command.");
+            return;
+        }
+        if (!voiceChannel.joinable) {
+            interaction.reply("I cannot join this voice channel.");
+            return;
+        }
+        youtube_player.join(voiceChannel);
+        interaction.reply(`Joined ${voiceChannel.name}`);
+    }
+
+    @Slash({ name: "leave", description: "leave a the channel the bot is in" })
+    async leave(interaction: CommandInteraction) {
+        youtube_player.leave();
+        interaction.reply("Left the voice channel.");
+    }
+
+    @Slash({ name: "play", description: "Play a youtube video" })
+    async play(
         @SlashOption({
-            description: "x value",
-            name: "x",
+            name: "link",
+            description: "the youtube link to play",
             required: true,
-            type: ApplicationCommandOptionType.Number,
+            type: ApplicationCommandOptionType.String,
         })
-        x: number,
-        @SlashOption({
-            description: "y value",
-            name: "y",
-            required: true,
-            type: ApplicationCommandOptionType.Number,
-        })
-        y: number,
+        link: string, // https://www.youtube.com/watch?v=XXXXXXXXXXX&... or https://youtu.be/XXXXXXXXXXX?...
         interaction: CommandInteraction
     ): Promise<void> {
-        interaction.reply(`${x} + ${y} = ${x + y}`);
+        const member = interaction.guild?.members.cache.get(interaction.user.id);
+        if (!member) {
+            interaction.reply("I can't find you");
+            return;
+        }
+        if (!member.voice.channel) {
+            interaction.reply("You need to be in a voice channel to use this command.");
+            return;
+        }
+
+        if (!youtube_player.getChannel()) {
+            youtube_player.join(member.voice.channel);
+        } else if (youtube_player.getChannel() !== member.voice.channel) {
+            console.log(youtube_player.getChannel(), member.voice.channel);
+            interaction.reply("I'm not in the same channel as you.");
+            return;
+        }
+
+        const regex = /(?:\/watch\?v=([^&\n ]+))|(?:youtu\.be\/([^?\n \/]+))/;
+        const match = link.match(regex);
+        if (!match) {
+            interaction.reply("Invalid YouTube link.");
+            return;
+        }
+        const video_id = match[1] || match[2];
+
+        // interaction.deferReply();
+        if (youtube_player.audioExists(video_id)) {
+            await interaction.reply(`Loading https://youtu.be/${video_id}`);
+        } else {
+            await interaction.reply(`Downloading https://youtu.be/${video_id}`);
+        }
+
+        youtube_player.addAudio(video_id).then(async () => {
+            await interaction.editReply(`Playing https://youtu.be/${video_id}`);
+            console.log(`Playing https://youtu.be/${video_id}`);
+        });
+    }
+
+    @Slash({ name: "pause", description: "Pause the current song" })
+    async pause(interaction: CommandInteraction) {
+        youtube_player.pause();
+        interaction.reply("Paused the current song.");
+    }
+
+    @Slash({ name: "resume", description: "Resume the current song" })
+    async resume(interaction: CommandInteraction) {
+        youtube_player.play();
+        interaction.reply("Resumed the current song.");
+    }
+
+    @Slash({ name: "skip", description: "Skip the current song" })
+    async skip(interaction: CommandInteraction) {
+        youtube_player.skip();
+        interaction.reply("Skipped the current song.");
+    }
+
+    @Slash({ name: "stop", description: "Stop the bot" })
+    async stop(interaction: CommandInteraction) {
+        youtube_player.clear();
+        youtube_player.leave();
+        interaction.reply("Stopped the bot.");
     }
 }
---
+++ src/utils/youtubePlayer.ts
@@ -0,0 +1,101 @@
+import {
+    AudioPlayer,
+    AudioPlayerStatus,
+    AudioResource,
+    createAudioPlayer,
+    createAudioResource,
+    joinVoiceChannel,
+    VoiceConnection,
+} from "@discordjs/voice";
+import { VoiceBasedChannel } from "discord.js";
+import { existsSync, mkdirSync } from "fs";
+import youtubeDl from "youtube-dl-exec";
+
+const directory = "./res/youtube";
+if (!existsSync(directory)) {
+    mkdirSync(directory, { recursive: true });
+}
+
+class YoutubePlayer {
+    private channel?: VoiceBasedChannel;
+    private connection?: VoiceConnection;
+    private player: AudioPlayer;
+    private songs: string[] = [];
+
+    constructor() {
+        this.player = createAudioPlayer();
+        this.player.on(AudioPlayerStatus.Idle || AudioPlayerStatus.AutoPaused, () => {
+            this.skip();
+        });
+    }
+
+    join(channel: VoiceBasedChannel): void {
+        if (this.connection) {
+            this.leave();
+        }
+        this.channel = channel;
+        this.connection = joinVoiceChannel({
+            channelId: this.channel.id,
+            guildId: this.channel.guild.id,
+            adapterCreator: this.channel.guild.voiceAdapterCreator,
+        });
+        this.connection.subscribe(this.player);
+        this.play();
+    }
+
+    leave(): void {
+        this.connection?.destroy();
+        this.connection = undefined;
+        this.channel = undefined;
+        this.clear();
+    }
+
+    async addAudio(video_id: string): Promise<void> {
+        if (!existsSync(`${directory}/${video_id}.mp3`)) {
+            await youtubeDl(`https://www.youtube.com/watch?v=${video_id}`, {
+                extractAudio: true,
+                audioFormat: "mp3",
+                output: `${directory}/${video_id}.mp3`,
+                noCheckCertificates: true,
+                noWarnings: true,
+            });
+        }
+
+        this.songs.push(video_id);
+        this.play();
+    }
+
+    play(): void {
+        this.player.unpause();
+        if (!this.isPlaying()) {
+            this.player.play(createAudioResource(`${directory}/${this.songs.shift()}.mp3`));
+        }
+    }
+
+    pause(): void {
+        this.player.pause();
+    }
+
+    skip(): void {
+        const song = this.songs.shift();
+        if (song) {
+            this.player.play(createAudioResource(`${directory}/${song}.mp3`));
+        } else {
+            this.leave();
+        }
+    }
+
+    clear(): void {
+        this.songs = [];
+        this.player.stop(true);
+    }
+
+    audioExists = (video_id: string) => existsSync(`${directory}/${video_id}.mp3`);
+    isPlaying = () => this.player.state.status === AudioPlayerStatus.Playing;
+
+    getChannel = () => this.channel;
+    getConnection = () => this.connection;
+    getPlayer = () => this.player;
+    getSongs = () => this.songs;
+}
+export default YoutubePlayer;
